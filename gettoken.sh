4efdbc93555df5cc88a62f70888ce5a15e17b61e
Charango7369 <edwinjrivero@gmail.com>
2026-05-03 02:08:17 -0400

    chore: eliminar scripts locales de gettoken (contienen credenciales)

--- gettoken.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-RESPONSE=$(curl -s -X POST http://127.0.0.1:8001/api/v1/auth/login \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "username=admin@apolodigital.lat&password=$1")
-echo "$RESPONSE" | grep -o '"access_token":"[^"]*"' | cut -d'"' -f4
